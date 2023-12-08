1a848bd53cf29831e1bf4a6908c7010153111a02
SithLor <84148006+SithLor@users.noreply.github.com>
2023-12-08 17:09:27 +0000

    lamo

--- scripts/lag_securly.ts
+++
@@ -1,8 +0,0 @@
-enum url {
-    ext = "https://extensions.securly.com/extenison.xml,
-    policy = "https://policy.securly.com/policy.xml",
-}
-function url_edit(email,reason:"domainblockedforuser",categoryid:50331904,policyid:2,keyword,extension_id:"knneimmpeamikmjijmnhjmmobddleohj"){
-
-    
-}
